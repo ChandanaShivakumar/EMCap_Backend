364dfc14d85653389a673e10edfdfb8fa95e5d70
ChandanaShivakumar <chandanashiva55@gmail.com>
2023-06-22 17:37:16 +0530

    Add files via upload

---
+++ EMS/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/', include('ems_api.urls')),
+    path('api/users/', include('emcaplogin.urls')),
+    
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
